f163ed634e454c0a3d6b1b6a960ed1386d1ecd4d
Carlo Namoca <carlonamoca@Carlos-MBP-2.hitronhub.home>
2018-04-23 16:41:20 -0700

    Added palindrome algorithm (not done)

---
+++ Palindromes.playground/Contents.swift
@@ -0,0 +1,32 @@
+// Understanding Palindromes in Swift
+// From https://github.com/raywenderlich/swift-algorithm-club/tree/master/Palindromes
+
+import Foundation
+
+func isPalindrome(str: String) -> Bool {
+    let strippedString = str.replacingOccurrences(of: "\\W", with: "", options: .regularExpression, range: nil)
+    let length = strippedString.count
+    
+    if length > 1 {
+        palindrome(str: strippedString.lowercased(), left: 0, right: length - 1)
+    }
+    
+    return false
+}
+
+func palindrome(str: String, left: Int, right: Int) -> Bool {
+    if left >= right {
+        return true
+    }
+    
+    let lhs = str[str.index(str.startIndex, offsetBy: left)]
+    let rhs = str[str.index(str.startIndex, offsetBy: right)]
+
+    if lhs != rhs {
+        return false
+    }
+    
+    return palindrome(str: str, left: left + 1, right: right + 1)
+}
+
+isPalindrome(str: "radar")
