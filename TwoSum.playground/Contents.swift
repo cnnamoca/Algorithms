b251f62eb9bd99a64dff6d8f0416b5a25fdad8aa
CarloNamoca <carlo.namoca@gmail.com>
2018-04-25 12:10:07 -0700

    Added Two Sum Algorithm

---
+++ TwoSum.playground/Contents.swift
@@ -0,0 +1,36 @@
+//Two Sum Problem from https://www.youtube.com/watch?v=cUmGngurKXo
+
+//The best way to go through this algorithm is by using pointers from the lowest end of the array and the highest end of the array
+
+//We also have to make sure that our array is sorted
+
+import Foundation
+
+let array = [1, 4, 6, 2, 6, 7, 12, 14]
+
+func twoSum(array: [Int], sum: Int) -> Bool {
+    
+    var lhs = 0
+    var rhs = array.count - 1
+    
+    while lhs < rhs {
+        
+        let sumOfItems = array[lhs] + array[rhs]
+        
+        if sumOfItems == sum {
+            return true
+        }
+        
+        if sumOfItems < sum {
+            lhs += 1
+        }
+        
+        if sumOfItems > sum {
+            rhs += 1
+        }
+    }
+    
+    return false
+}
+
+twoSum(array: array, sum: 8)
